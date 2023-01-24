972cc4265c4e6d07179dfafbed6dbd40302d6a57
Han511 <100351355+Han511@users.noreply.github.com>
2023-01-24 16:57:18 +0530

    Add files via upload
    to specify whether the number is negative, positive, or zero.

---
+++ Positive,Negative,Zero/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int a;
+        printf("Enter integer number");
+        scanf("%d",&a);
+    if(a>0);
+       {printf("The given integer number is positive number.");}
+    if(a<0);
+        {printf("The given integer number is negative number. ");}
+        if(a==0);
+        {printf("The given number is neither positive nor negative.");}
+    return 0;
+}
