d2feaa6a2fbebbb99ffa20339621d7a1dd5b190c
Han511 <100351355+Han511@users.noreply.github.com>
2023-01-24 16:41:30 +0530

    Add files via upload

---
+++ call by reeference/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void change(int *num)
+    {
+        printf("Before adding value inside function name=%d\n",*num);
+        (*num)+=100;
+       printf("After adding value inside function name=%d\n",*num);
+
+    }
+int main()
+    {
+        printf("NAME:HANA ALEBACHEW MENGIST\nROLL NO.:92000103161\nCLASS: TC4\n\n");
+        int x=100;
+        printf("Before function call x=%d\n",x);
+        change(&x);
+        printf("After function call x=%d\n",x);
+
+    return 0;
+   }
+
